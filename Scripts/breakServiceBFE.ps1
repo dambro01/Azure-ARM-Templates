00f9248394be0ec45292a3d2d88a653aff2123da
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-10-07 18:15:51 +0100

    Add files via upload

---
+++ Scripts/breakServiceBFE.ps1
@@ -0,0 +1,20 @@
+# Disables inheritance
+
+$registry = "HKLM:\SYSTEM\CurrentControlSet\Services\BFE"
+$acl = Get-Acl $registry
+$DisableInheritance=$true
+$PreserveInheritanceIfDisabled =$True
+$acl.SetAccessRuleProtection($DisableInheritance, $preserveInheritanceIfDisabled)
+$acl | Set-Acl -Path $registry
+
+# Break the permissions
+
+$registry = "HKLM:\SYSTEM\CurrentControlSet\Services\BFE\Parameters"
+$acl = Get-Acl $registry
+$id = "BUILTIN\Users"
+$rule = New-Object System.Security.AccessControl.RegistryAccessRule ($id,"FullControl","Deny")
+$id1 = "BUILTIN\Administrators"
+$rule1 = New-Object System.Security.AccessControl.RegistryAccessRule ($id1,"FullControl","Deny")
+$acl.SetAccessRule($rule)
+$acl.SetAccessRule($rule1)
+$acl | Set-Acl -Path $registry
