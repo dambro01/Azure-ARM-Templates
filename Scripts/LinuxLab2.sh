8a085e010c3f0d711df5a180e39a8c6f1cc7f944
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-10-13 10:03:38 -0400

    Activate LinuxLab2

--- Scripts/LinuxLab2.sh
+++ Scripts/LinuxLab2.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+# Allow SELINUX to manage port [DEBUG]
+# yum install -y policycoreutils-python-utils
+# semanage port -a -t ssh_port_t -p tcp 22
+
+# Disable SELinux
+setenforce 0
+
+# Update sshd_config
+sed -i 's/\#Port 22/Port 2222\;/' /etc/ssh/sshd_config
+
+# Start firewalld and enable service at system startup
+systemctl start firewalld
+systemctl enable firewalld
+
+# Reboot
+shutdown -r
