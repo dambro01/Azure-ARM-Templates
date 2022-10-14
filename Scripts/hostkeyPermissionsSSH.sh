1d6d4a2cf31c0ab8dc29c618167faad0b2949d8c
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-10-14 11:51:29 +0100

    Update hostkeyPermissionsSSH.sh

--- Scripts/hostkeyPermissionsSSH.sh
+++ Scripts/hostkeyPermissionsSSH.sh
@@ -4,15 +4,3 @@ sudo chmod 777 /etc/ssh/ssh_host*key
 sudo chmod 777 /etc/ssh/ssh_host*key.pub
 sudo chmod 777 /etc/ssh/sshd_config
 shutdown -r
-
-
-
-chmod -R 600 /etc/ssh
-chmod 700 /etc/ssh/ssh_host*key
-chmod 644 /etc/ssh/ssh_host*key.pub
-chmod 640 /etc/ssh/sshd_config
-chmod 755 /var/lib/empty
-
-
-
-https://supportability.visualstudio.com/AzureIaaSVM/_wiki/wikis/AzureIaaSVM/495366/Server-Unexpectedly-Closed-the-Network-Connection_RDP-SSH
