c70fe26eabcaff9ab9772bae23a4e5dbd5df0bc7
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-10-18 18:34:33 +0100

    Delete blackScreenRDP.ps1
    Lab not working

--- Scripts/blackScreenRDP.ps1
+++
@@ -1,8 +0,0 @@
-# Removes NT AUTHORITY\INTERACTIVE and NT AUTHORITY\Authenticated Users from local group Users
-
-Remove-LocalGroupMember -Group "Users" -Member "NT AUTHORITY\INTERACTIVE"
-Remove-LocalGroupMember -Group "Users" -Member "NT AUTHORITY\Authenticated Users"
-
-# Restarts the VM
-
-Restart-Computer -Force
