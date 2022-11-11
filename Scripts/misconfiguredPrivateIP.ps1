9b55df2b9da1a5953b4628c7a6c27bd7e98a1ade
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-11-11 18:21:24 +0000

    Delete misconfiguredPrivateIP.ps1

--- Scripts/misconfiguredPrivateIP.ps1
+++
@@ -1,6 +0,0 @@
-# Changes the IP inside at the OS level
-
-$nic = Get-NetAdapter | where {$_.InterfaceDescription -like "Microsoft Hyper-V*"}
-Set-DnsClientServerAddress -InterfaceIndex $nic.ifIndex -ServerAddresses ("168.63.129.16")
-Set-NetIPAddress -InterfaceIndex $nic.ifIndex -PrefixLength 24
-New-NetIPAddress -InterfaceIndex $nic.ifIndex -IPAddress "192.168.3.2" -DefaultGateway "192.168.3.1" -PrefixLength 24 -AddressFamily IPv4
