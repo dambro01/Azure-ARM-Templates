b52f63faa02474b35709a3f0ecc857a916756ba0
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-12-27 18:12:11 +0000

    Update tempProfile.ps1

--- Scripts/tempProfile.ps1
+++ Scripts/tempProfile.ps1
@@ -1,6 +1,6 @@
 # Corrupts the user profie
 
-Start-Sleep -Seconds 120
+Start-Sleep -Seconds 60
 
 $username = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\username.txt" -First 1
 $password = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\password.txt" -First 1
