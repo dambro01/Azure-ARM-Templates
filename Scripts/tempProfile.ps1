3c022c728f012d1a866fff19e1b298518a227e00
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-12-27 16:20:28 +0000

    Add files via upload

---
+++ Scripts/tempProfile.ps1
@@ -0,0 +1,18 @@
+# Corrupts the user profie
+
+md c:\temp
+
+cd C:\temp\
+$registry = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" -Recurse
+Foreach($a in $registry) {
+    $a.Property | ForEach-Object {
+        If($a.GetValue($_) -eq ("C:\Users\" + $username))
+        {
+            $path = $a.PSChildName
+            Invoke-Command { reg export ("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" + $path) regbck.reg }
+            Rename-Item -Path ("HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" + $path) -NewName ($path + ".bak")
+            Invoke-Command { reg import regbck.reg }
+            Set-ItemProperty -Path ("HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\" + $path) -name "ProfileImagePath" -value ("C:\Users\" + $username + "1")
+            }
+    }
+}
