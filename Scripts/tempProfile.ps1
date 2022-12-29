00d4a30915757f101cdc6b96d485c9615b9e0ba1
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-12-29 13:26:43 +0000

    Update tempProfile.ps1

--- Scripts/tempProfile.ps1
+++ Scripts/tempProfile.ps1
@@ -1,8 +1,5 @@
 # Corrupts the user profie
 
-#$username = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\username.txt" -First 1
-#$password = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\password.txt" -First 1
-
 $username = "azuresota"
 $password = "P@ssword!123"
 
