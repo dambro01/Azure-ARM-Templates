e42bc982f581adab1106958bb20f77709f59a7a8
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2022-12-29 12:56:35 +0000

    Update tempProfile.ps1

--- Scripts/tempProfile.ps1
+++ Scripts/tempProfile.ps1
@@ -1,6 +1,6 @@
 # Corrupts the user profie
 
-#$username = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\username.txt" -First 1
+$username = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\username.txt" -First 1
 #$password = Get-Content "C:\Packages\Plugins\Microsoft.CPlat.Core.RunCommandHandlerWindows\2.0.5\Downloads\password.txt" -First 1
 
 #$Action = New-ScheduledTaskAction -Execute "notepad.exe"
