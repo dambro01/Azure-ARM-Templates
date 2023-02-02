9a438ff9fafb4c7061c23a0707d21efef664ed44
Carlos Sota <96530403+CarlSota@users.noreply.github.com>
2023-02-02 13:12:04 +0000

    Add files via upload

---
+++ Scripts/migratedVmOnPrem.sh
@@ -0,0 +1,17 @@
+cp /etc/default/grub /tmp/grub.old
+
+> /etc/default/grub
+
+echo 'GRUB_TIMEOUT=5' >> /etc/default/grub
+echo 'GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"' >> /etc/default/grub
+echo 'GRUB_DEFAULT=saved' >> /etc/default/grub
+echo 'GRUB_DISABLE_SUBMENU=true' >> /etc/default/grub
+echo 'GRUB_TERMINAL_OUTPUT="console"' >> /etc/default/grub
+echo 'GRUB_CMDLINE_LINUX="crashkernel=auto rhgb quiet"' >> /etc/default/grub
+echo 'GRUB_DISABLE_RECOVERY="true"' >> /etc/default/grub
+
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+sed -i 's/BOOTPROTO=dhcp/BOOTPROTO=static/' /etc/sysconfig/network-scripts/ifcfg-eth0
+
+reboot
